4399864a08c37de6f207f6e8bdf28657f6e34b8c
David Felix <davidffelix@hotmail.com>
2023-04-03 17:53:32 -0300

    first commit

---
+++ lib/src/imc/imc_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ImcPage extends StatefulWidget {
+  const ImcPage({super.key});
+
+  @override
+  State<ImcPage> createState() => _ImcPageState();
+}
+
+class _ImcPageState extends State<ImcPage> {
+  var height = 0.0;
+
+  var weight = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('IMC Page'),
+      ),
+      body: Column(
+        children: [
+          TextFormField(
+            onChanged: (value) {
+              height = double.parse(value);
+            },
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Altura',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
